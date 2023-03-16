c7689f4dce875acaaf1ff49012344537feccbfc5
Kunal Chhetri <kunalchhetri14@gmail.com>
2023-03-16 23:46:05 +0530

    feature(service): Initial folder setup and rest controller and value picked from application.properties

---
+++ 01-SpringBootOverview/01-dev-tools-demo/src/main/java/com/kunal/springboot/SampleApp/rest/HelloController.java
@@ -0,0 +1,19 @@
+package com.kunal.springboot.SampleApp.rest;
+
+import com.kunal.springboot.SampleApp.SampleAppApplication;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+  @Value("owner.name")
+  private String name;
+
+  @GetMapping("/")
+  public String sayHello() {
+    System.out.println("THe name is : "+ name);
+    return "Hellow World";
+  }
+}
