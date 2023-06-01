75cf6d5e077969df7b0f03491fe01f852186e28e
Kunal Chhetri <kunalchhetri14@gmail.com>
2023-06-01 14:54:14 +0530

    fix(app): constructor injection for multiple Coach types

--- 02-spring-boot-core/01-constructor-injection/src/main/java/com/kunal/springBootDependencyInjection/SpringApp/controller/DemoController.java
+++ 02-spring-boot-core/01-constructor-injection/src/main/java/com/kunal/springBootDependencyInjection/SpringApp/controller/DemoController.java
@@ -1,6 +1,8 @@
 package com.kunal.springBootDependencyInjection.SpringApp.controller;
 
 import com.kunal.springBootDependencyInjection.SpringApp.model.coach.Coach;
+import com.kunal.springBootDependencyInjection.SpringApp.model.coach.CricketCoach;
+import com.kunal.springBootDependencyInjection.SpringApp.model.coach.FootballCoach;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -9,17 +11,24 @@ import org.springframework.web.bind.annotation.RestController;
 public class DemoController {
 
   //define a private field for the dependency
-  private Coach coach;
+  private Coach cricketCoach;
+  private Coach footballCoach;
 
   //define a constructor for dependency injection
   @Autowired
-  DemoController(Coach coach) {
-    this.coach = coach;
+  DemoController(CricketCoach cricketcoach, FootballCoach footballCoach) {
+    this.cricketCoach = cricketcoach;
+    this.footballCoach = footballCoach;
   }
 
-  @GetMapping("/dailyWorkout")
-  public String getWorkout() {
-    return this.coach.getDailyWorkout();
+  @GetMapping("/dailyWorkoutCricket")
+  public String getCricketWorkout() {
+    return this.cricketCoach.getDailyWorkout();
+  }
+
+  @GetMapping("/dailyWorkoutFootball")
+  public String getFootballWorkout() {
+    return this.footballCoach.getDailyWorkout();
   }
 
 }
