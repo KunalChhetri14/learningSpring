6458e91c793e09e963007311e980da35b04ff813
Kunal Chhetri <kunalchhetri14@gmail.com>
2023-06-07 02:02:51 +0530

    Checking default scope i.e prototype scope

--- 02-spring-boot-core/07-bean-scopes/src/main/java/com/kunal/springBootDependencyInjection/SpringApp/model/coach/CricketCoach.java
+++ 02-spring-boot-core/07-bean-scopes/src/main/java/com/kunal/springBootDependencyInjection/SpringApp/model/coach/CricketCoach.java
@@ -1,9 +1,13 @@
 package com.kunal.springBootDependencyInjection.SpringApp.model.coach;
 
+import org.springframework.beans.factory.annotation.Configurable;
+import org.springframework.beans.factory.config.ConfigurableBeanFactory;
 import org.springframework.context.annotation.Primary;
+import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Component;
 
 @Component
+@Scope(ConfigurableBeanFactory.SCOPE_PROTOTYPE)
 public class CricketCoach implements Coach{
 
   CricketCoach() {
